aee1a7c0830024042d0407bdb104e4d15c0a0980
xmllovemm <xiemaolin@126.com>
2015-05-27 09:46:22 +0800

    add interface

---
+++ Header.h
@@ -0,0 +1,26 @@
+//
+//  Header.h
+//  MysqlConnector
+//
+//  Created by lemon on 5/27/15.
+//  Copyright (c) 2015 personal. All rights reserved.
+//
+
+#ifndef MysqlConnector_Header_h
+#define MysqlConnector_Header_h
+
+class MySqlConnector {
+    
+    
+public:
+    MySqlConnector(const char* ip, int port, const char* db)
+    {
+        
+    }
+    
+private:
+    
+};
+
+
+#endif
